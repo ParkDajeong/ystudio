5d15ce085a802aa6a1aaf8497ec9e5534910f426
ParkDajeong <sobeast980@gmail.com>
2021-02-04 10:15:26 +0900

    fix header 구현

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 "use strict";
 
+// FixHeader
+function fixHeader(scroll) {
+  if(scroll > 150) {
+    $(".main-header").addClass("on");
+  } else {
+    $(".main-header").removeClass("on");
+  }
+}
+
+$(window).on("scroll resize", function() {
+  const scroll = $(document).scrollTop();
+  fixHeader(scroll);
+});
+
+// Global nav
 $(function() {
   $(".main-header__btn-menu-open, .gnb__btn-menu-close").on("click", function() {
     $(".gnb").toggleClass("on");
